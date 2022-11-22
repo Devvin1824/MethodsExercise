c4f692b0d285b412e23c8cfa46ca7a8da8e7901d
Devin1824 <devster18@gmail.com>
2022-11-22 08:49:06 -0800

    initial commit

---
+++ MethodsExercise/Functions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsExercise
+{
+    internal class Functions
+    {
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+       
+        public static int Multiply(int num3, int num4)
+        {
+            return num3 * num4;
+        }
+
+        
+        public static int AddChallenge(params int[] list)
+        {
+            int sum = 0;
+            for (int i = 0; i < list.Length; i++)
+            {
+                sum += list[i]; 
+            }
+            return sum;
+            
+        }
+    }
+
+}
--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your favorite band?");
+            string band = Console.ReadLine();
+            Console.WriteLine("what is your favorite color?");
+            string color = Console.ReadLine();
+            Console.WriteLine("what is your favorite animal?");
+            string animal = Console.ReadLine();
+            
+            Console.WriteLine($"One day, sometime after lunch but not quite yet dinner time, {name} was walking to their favorite store (CVS Pharmacy) \n" +
+                $"while listening to their favorite band {band} on their brand new {color} ipod shuffle. When they were about 20 feet away from getting \n" +
+                $"to the store, a {animal} ran into them and knocked them into another dimension. To this day, nobody has seen or heard from {name}.");
+
+            int sum;
+            int product;
+
+            sum = Functions.Add(2, 4);
+            product = Functions.Multiply(10, 2);
+            Console.WriteLine($"The sum is {sum}.");
+            Console.WriteLine($"The product is {product}.");
+            
+            Console.WriteLine(Functions.AddChallenge(3, 6, 7));
+           
         }
     }
 }
